334833b8dcbe6dca5f8f5af12f92ecb5e3c3bfda
Yuriy Suhak <iri13ua@gmail.com>
2019-08-27 23:06:09 +0300

    start_gallery

--- js/script.js
+++ js/script.js
@@ -12,4 +12,29 @@ $(".cervices-bar").on('click', 'li', function() {
     marker = $(this).index();
     $OurS[marker].hidden = false;
 });
-//
+// Our Amazing Work
+const $gallery = $("#picsBlock>img");
+
+let amount = 12;
+
+$("#morePics").on('click', function () {
+    amount += 12;
+    console.log(amount);
+    debugger;
+    addPics($gallery, amount);
+} );
+
+function addPics(arr, amount) {
+
+    for (let i = 0; i < (amount || arr.length); ++i) {
+        arr[i].hidden = false
+    }
+}
+
+function removePics(arr) {
+    for (let i = 0; i < arr.length; ++i) {
+        arr[i].hidden = true
+    }
+}
+removePics($gallery);
+addPics($gallery, amount);
