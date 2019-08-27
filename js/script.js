c62de66a9dc416bc5080a0a7b8f306b08d52e45b
Yuriy Suhak <iri13ua@gmail.com>
2019-08-27 21:06:23 +0300

    +js_tabs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-const hr = document.getElementsByClassName("hr");
+// Our Services
+const $OurS = $(".article-block>div");
+for (let i = 1; i < $OurS.length; ++i) {
+    $OurS[i].hidden = true
+}
+let marker = 0;
+
+$(".cervices-bar").on('click', 'li', function() {
+    $(".cervices-bar>li").removeClass("delta");
+    $(this).addClass("delta");
+    $OurS[marker].hidden = true;
+    marker = $(this).index();
+    $OurS[marker].hidden = false;
+});
+//
